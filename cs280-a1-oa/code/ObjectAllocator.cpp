c7bf6f4852889d1e447d3281193c3f6409f10610
benjamin <benjaminzhiyuan.lee@digipen.edu>
2024-01-31 20:42:12 +0800

    object allocator

---
+++ cs280-a1-oa/code/ObjectAllocator.cpp
@@ -0,0 +1,11 @@
+/*!******************************************************************
+ * \file      ObjectAllocator.cpp
+ * \author    Benjamin Lee
+ * \par       DP email: benjaminzhiyuan.lee\@digipen.edu.sg
+ * \par       Course: CSD280
+ * \par       Section: A
+ * \par      
+ * \date      31-01-2024
+ * 
+ * \brief  
+ *********************************************************************/
--- cs280-a1-oa/code/ObjectAllocator.h
+++ cs280-a1-oa/code/ObjectAllocator.h
@@ -1,3 +1,15 @@
+/*!******************************************************************
+ * \file      ObjectAllocator.h
+ * \author    Benjamin Lee
+ * \par       DP email: benjaminzhiyuan.lee\@digipen.edu.sg
+ * \par       Course: CSD280
+ * \par       Section: A
+ * \par      
+ * \date      31-01-2024
+ * 
+ * \brief  
+ *********************************************************************/
+
 //---------------------------------------------------------------------------
 #ifndef OBJECTALLOCATORH
 #define OBJECTALLOCATORH
